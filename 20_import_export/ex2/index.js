b56f9cd6d8198357b4b28d0a2787eb2339a08241
Cheder-one <cherbadenis1@gmail.com>
2023-02-06 21:24:12 +0300

    u

--- 20_import_export/ex2/index.js
+++ 20_import_export/ex2/index.js
@@ -1,8 +1,14 @@
 import './index.css';
 
 const cookieConsentButton = document.querySelector('.cookie-consent__button');
+const cookieConsent = document.querySelector('.cookie-consent');
+const isCookieClick = localStorage.getItem('buttonClicked');
 
-cookieConsentButton.addEventListener('click', (event) => {
-   const { target } = event
+cookieConsentButton.addEventListener('click', function() {
+   localStorage.setItem('buttonClicked', 'true');
+   cookieConsent.remove();
+});
 
-})
+if (isCookieClick === 'true') {
+   cookieConsent.remove();
+}
