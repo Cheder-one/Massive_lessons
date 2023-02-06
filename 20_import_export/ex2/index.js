3e1162ab800cc832d64cfbf5055fb769bf3fa060
Cheder-one <cherbadenis1@gmail.com>
2023-02-06 20:43:45 +0300

    u

--- 20_import_export/ex2/index.js
+++ 20_import_export/ex2/index.js
@@ -1,1 +1,8 @@
 import './index.css';
+
+const cookieConsentButton = document.querySelector('.cookie-consent__button');
+
+cookieConsentButton.addEventListener('click', (event) => {
+   const { target } = event
+
+})
